d6afa3a67278a76861de6e1145de8423381f8776
pizzahuhthp9 <npwchr.work@gmail.com>
2021-06-08 18:09:37 +0700

    add method binding between components

--- src/views/Graph.jsx
+++ src/views/Graph.jsx
@@ -5,14 +5,18 @@ import GraphInput from '../components/graph/GraphInput'
 class Graph extends Component {
     constructor(props) {
         super(props);
+        this.addData = this.addData.bind(this);
         this.state = {  }
     }
     render() { 
         return ( 
             <div>
-                <GraphInput></GraphInput>
+                <GraphInput submit={this.addData}></GraphInput>
             </div>
          );
+    }
+    addData(){
+        console.log("data added");
     }
 }
  
