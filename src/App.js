32be380bf27d2f675ccd9bdd4bc2015e657ab260
pizzahuhthp9 <npwchr.work@gmail.com>
2021-06-09 00:46:05 +0700

    rework requiremet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Navbar from './components/navbar/Navbar'
+import Graph from './views/Graph'
 
 function App() {
   return (
     <div className="App my-4 mx-24">
-      <Navbar></Navbar>
+      <Graph></Graph>
     </div>
   );
 }
